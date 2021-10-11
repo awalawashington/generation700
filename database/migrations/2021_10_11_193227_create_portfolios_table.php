67c40c07bae38e76735a699973496bf068ae70c6
awalawashington <washingtonawala32@gmail.com>
2021-10-11 23:11:00 +0300

    auth and migrations

---
+++ database/migrations/2021_10_11_193227_create_portfolios_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePortfoliosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->unsignedBigInteger('portfolio_category_id')->index();
+            $table->timestamps();
+
+            $table->foreign('portfolio_category_id')->references('id')->on('portfolio_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+}
---
+++ database/migrations/2021_10_11_193621_create_blog_posts_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogPostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_posts', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('image');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->string('slug')->nullable();
+            $table->boolean('is_live')->default(false);
+            $table->string('disk')->default('public');
+            $table->boolean('upload_successful')->default(false);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_posts');
+    }
+}
